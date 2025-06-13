556a9ffaa555600672919f1efb3bbe017a225a6d
Gunjan Choukade <gunjanchoukade298@gmail.com>
2025-06-13 16:58:37 +0530

    await added

--- SERVER/Config/ConnectToDb.js
+++ SERVER/Config/ConnectToDb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectionToDB = async ()=>{
-    mongoose.connect(process.env.DB_URL).then(()=>{
+    await mongoose.connect(process.env.DB_URL).then(()=>{
         console.log("Connected to the Database");
     })
     .catch((err)=>{
